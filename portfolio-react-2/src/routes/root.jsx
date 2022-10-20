a63777f326118592fc2fd7211cc4bb17eb4228c0
Amanda Toop <60271855+atoopdev@users.noreply.github.com>
2022-10-20 10:58:56 -0700

    root.jsx on main view

---
+++ portfolio-react-2/src/routes/root.jsx
@@ -0,0 +1,8 @@
+export default function Root(){
+    return(
+        <div>
+            <h1>I am in src/routes/root.jsx</h1>
+            <p>When completed, this will be the main landing page.</p>
+        </div>
+    )
+}
