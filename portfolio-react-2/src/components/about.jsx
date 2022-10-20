3c37c08fc63b9dccf9b28b74875bde254550f1eb
Amanda Toop <60271855+atoopdev@users.noreply.github.com>
2022-10-20 13:32:20 -0700

    Beginning of About()

---
+++ portfolio-react-2/src/components/about.jsx
@@ -0,0 +1,11 @@
+
+import AmandaImg from "../images/portfolio_header.jpg"
+
+export default function About(){
+    return <header className="About">
+        <div className="content_wrapper">
+            <h1>Amanda Toop</h1>
+            <h2>Information Technology Specialist</h2>
+        </div>
+    </header>;
+}
--- portfolio-react-2/src/routes/root.jsx
+++ portfolio-react-2/src/routes/root.jsx
@@ -1,8 +1,12 @@
+import React from "react"
+import About from "../components/about"
+
 export default function Root(){
     return(
         <div className="content_wrapper">
             <h1>I am in src/routes/root.jsx</h1>
             <p>When completed, this will be the main landing page.</p>
+            <About />
         </div>
     )
 }
