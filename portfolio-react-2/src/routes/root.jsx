52c33724f07adbccc88680ef0a998516753183a7
Amanda Toop <60271855+atoopdev@users.noreply.github.com>
2022-10-21 14:56:13 -0700

    basic linking

--- portfolio-react-2/src/routes/root.jsx
+++ portfolio-react-2/src/routes/root.jsx
@@ -2,7 +2,7 @@ import React from "react"
 import About from "../components/about"
 import Nav from "../components/nav"
 
-import {Outlet} from "react-router-dom"
+import {Outlet, Link} from "react-router-dom"
 
 // functions like main container for modules to load in
 
@@ -14,6 +14,10 @@ export default function Root(){
         <div className="content_wrapper">
             <p>I am in src/routes/root.jsx</p>
             <p>When completed, this will be the main landing page.</p>
+            <p>Testing links</p>
+            <ul>
+                <li><Link to={`Projects/`}>Projects</Link></li>
+            </ul>
             {/* Outlet is where content renders */}
            <Outlet />
         </div>
