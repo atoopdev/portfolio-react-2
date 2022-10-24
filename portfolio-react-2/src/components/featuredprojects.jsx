3ac67cfc97e749d1309e0acba57aa2a2838fbee7
Amanda Toop <60271855+atoopdev@users.noreply.github.com>
2022-10-24 13:52:33 -0700

    Loading featuredProjects to homepage

--- portfolio-react-2/src/components/featuredprojects.jsx
+++ portfolio-react-2/src/components/featuredprojects.jsx
@@ -1,11 +1,27 @@
 import React from "react"
 import ProjectData from "../data"
 import ProjectCard from "./projectcard"
+import {Link} from "react-router-dom"
 
 export default function FeaturedProjects(){
+    const projectElements = ProjectData.map((project)=>{
+        if(project.isFeatured===true){
+            return <ProjectCard
+            key={project.id}
+            project={project} />
+        }
+    })
+
+    console.log("projectElements: ", projectElements)
+
+
     return (
-        <div>
-            <h1>In Featured Projects</h1>
+        <div className="featured-projects">
+            <section className="content_wrapper">
+                <h2>Featured projects</h2>
+                <p>View selected projects below. Complete list of projects can be found at <Link to="projects/">https://www.amandatoop.com/projects/</Link>.</p>
+                {projectElements}
+            </section>
         </div>
     )
 }
