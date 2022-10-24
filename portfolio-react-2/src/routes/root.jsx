7c2191bc41b34e33359e36e293b35c2c6e7898c4
Amanda Toop <60271855+atoopdev@users.noreply.github.com>
2022-10-24 11:30:32 -0700

    added useNavigation() to catch loading state

--- portfolio-react-2/src/routes/root.jsx
+++ portfolio-react-2/src/routes/root.jsx
@@ -1,4 +1,5 @@
 import React from "react"
+import { useNavigation, } from "react-router-dom";
 import About from "../components/about"
 import Nav from "../components/nav"
 
@@ -8,11 +9,16 @@ import {Outlet, Link} from "react-router-dom"
 // functions like main container for modules to load in
 
 export default function Root(){
+
+    const navigation = useNavigation();
     return(
         <body>
            <Nav />  
         <main>
-        <div>
+        {/* useNavigation retruns current navigation state - idle, submitting, or loading
+        If site is not idle, a loading class is added to main part of app
+        Must use css to make use of this feature */}
+        <div id="detail" className={navigation.state === "loading" ? "loading" : ""}>
             <p>I am in src/routes/root.jsx</p>
             <p>When completed, this will be the container page.</p>
             
