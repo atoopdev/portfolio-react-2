79dffbae174fb0ebbeb5bed8a49fccd24faa2ad0
Amanda Toop <60271855+atoopdev@users.noreply.github.com>
2022-10-25 14:09:11 -0700

    revert project card class names

--- portfolio-react-2/src/components/projectcard.jsx
+++ portfolio-react-2/src/components/projectcard.jsx
@@ -1,13 +1,13 @@
 import React from "react"
 
 export default function ProjectCard(props){
-    return <div className="project-card">
+    return <section className="project-item">
         <h3 className="project-title">{props.project.title}</h3>
-        <a href={props.project.link} className="project-btn"><img src={props.project.img} alt={props.project.title} className="projectcard-img"></img></a>
+        <a href={props.project.link} className="project-btn"><img src={props.project.img} alt={props.project.title} className="project-img"></img></a>
         <p className="project-description">{props.project.description}</p>
 
         {/* conditional display if there is blog link */}
         {props.project.blogLink && <p>Read more about my experience <a href={props.project.blogLink} className="blog-link">on my blog.</a></p>}
         <a href={props.project.link} className="btn">View Project</a>
-    </div>
+    </section>
 }
