735bb0be94439301fd7cd356d6b6dc511374fc81
Amanda Toop <60271855+atoopdev@users.noreply.github.com>
2022-10-26 13:44:55 -0700

    Add projects page classnames to projects.jsx

--- portfolio-react-2/src/routes/projects.jsx
+++ portfolio-react-2/src/routes/projects.jsx
@@ -2,6 +2,8 @@ import React from "react"
 import ProjectData from "../data"
 import ProjectCard from "../components/projectcard"
 import {Link} from "react-router-dom"
+import ProjectsIntro from "../components/projectsintro"
+import ContactInfo from "../components/contactinfo"
 
 export default function Root(){
     const projectElements = ProjectData.map((project)=>{
@@ -13,17 +15,22 @@ export default function Root(){
     console.log("projectElements: ", projectElements)
 
     return(
-        <div className="content_wrapper">
-            <h1>I am in src/routes/projects.jsx</h1>
-            <p>When completed, this will be the project page.</p>
-
         <div className="">
-            <section className="content_wrapper divider">
-                <h2>Featured projects</h2>
-                <p>View selected projects below. Complete list of projects can be found at <Link to="projects/">https://www.amandatoop.com/projects/</Link>.</p>
+            {/* <h1>I am in src/routes/projects.jsx</h1>
+            <p>When completed, this will be the project page.</p> */}
+            <ProjectsIntro />
+
+        <div className="projects">
+            <section className="content_wrapper">
+                <div className="projects-container">
+                <h2>Completed Projects</h2>
+                <div className="projects-list divider">
                 {projectElements}
+                </div>
+                </div>
             </section>
         </div>
+        <ContactInfo />
         </div>
     )
 }
