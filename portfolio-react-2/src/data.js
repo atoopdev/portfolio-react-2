a874b6f38b6beae6ac2d6411b681cdea19029ddc
Amanda Toop <60271855+atoopdev@users.noreply.github.com>
2022-10-26 12:17:44 -0700

    fixed relative file url paths

--- portfolio-react-2/src/data.js
+++ portfolio-react-2/src/data.js
@@ -1,10 +1,13 @@
+// note standard relative URL's do not work with react router dom. Will be relative to url not file location.
+// see stackoverflow.com/questions/37644265/correct-path-for-img-on-react-js
+
 export default[
     {
         id: 1,
         title: "BBQ Website",
         description: "Responsive website featuring HTML form styling and JavaScript for submission confirmation.",
-        img:"images/bbq-website.jpg",
-        link: "projects/scrimba/bbq/bbq.html",
+        img:"/images/bbq-website.jpg",
+        link: "/projects/scrimba/bbq/bbq.html",
         blogLink: null,
         isFeatured: true
     },
@@ -12,8 +15,8 @@ export default[
         id: 2,
         title: "Role Playing Game",
         description: "My most complex JavaScript app to date.Simple dice based role playing game featuring multiple monsters to defeat. Utilized classes, Object. assign(), constructor, external resource files, setTimeout(), arrow functions, .map(), and .join().",
-        img: "images/rpg.jpg",
-        link: "projects/scrimba/rpg/",
+        img: "/images/rpg.jpg",
+        link: "/projects/scrimba/rpg/index.html",
         blogLink: null,
         isFeatured: true 
     }
@@ -22,8 +25,8 @@ export default[
         id: 3,
         title: "Color Scheme Generator",
         description: "The app takes a seed hexadecimal color and color palette selection via a form and then requests a color palette from The Color API. The Color API returns a data object which is then used to populate the provide color palette.",
-        img: "images/color-scheme.jpg",
-        link: "projects/scrimba/color-scheme-generator/colorschemegen.html",
+        img: "/images/color-scheme.jpg",
+        link: "/projects/scrimba/color-scheme-generator/colorschemegen.html",
         blogLink: null,
         isFeatured: true 
     }
@@ -32,8 +35,8 @@ export default[
         id: 4,
         title: "Movie Watchlist",
         description: "This app allows a user to search for movies based on title using the OMDB API. The OMDB API returns a list of the top ten movie matches. Users can then add/remove movies from a watchlist that is saved in localStorage.",
-        img:"images/movie-watchlist.jpg",
-        link: "projects/scrimba/movie-watchlist/",
+        img:"/images/movie-watchlist.jpg",
+        link: "/projects/scrimba/movie-watchlist/index.html",
         blogLink: null,
         isFeatured: true 
     }
@@ -42,8 +45,8 @@ export default[
         id: 5,
         title: "War",
         description: "This app allows a user to search for movies based on title usinMy first foray into API's. This app uses the Deck of Cards API to run a simulation of the card game War. Utilized .fetch() and .json().g the OMDB API. The OMDB API returns a list of the top ten movie matches. Users can then add/remove movies from a watchlist that is saved in localStorage.",
-        img:"images/war2.jpg",
-        link: "projects/scrimba/War/",
+        img:"/images/war2.jpg",
+        link: "/projects/scrimba/War/index.html",
         blogLink: null,
         isFeatured: false 
     }
@@ -52,8 +55,8 @@ export default[
         id: 6,
         title: "Image Carousel",
         description: "Here is a skratch coded image carosel. The goal of this project was to interate forward and back through an array of images, showing only a single desired image at time.",
-        img:"images/carousel1.jpg",
-        link: "projects/scrimba/javascript-challenges/carousel.html",
+        img:"/images/carousel1.jpg",
+        link: "/projects/scrimba/javascript-challenges/carousel.html",
         blogLink: null,
         isFeatured: false 
     }
@@ -62,8 +65,8 @@ export default[
         id: 7,
         title: "Search Bar",
         description: "This project searches through an unordered list and then hides elements that do not match the search string one by one. This Javascript project used similar code as my simple Hide/Reveal modal.",
-        img:"images/searchbar.jpg",
-        link: "projects/scrimba/javascript-challenges/searchbar.html",
+        img:"/images/searchbar.jpg",
+        link: "/projects/scrimba/javascript-challenges/searchbar.html",
         blogLink: null,
         isFeatured: false 
     }
@@ -72,8 +75,8 @@ export default[
         id: 8,
         title: "My Emojis",
         description: "This is an emoji display app. This app allows the user to add an emoji of their choice to the beginning or end of the displayed emoji list via an input field. Emojis may also be removed from the beginning or end of a list.",
-        img:"images/emojis.jpg",
-        link: "projects/scrimba/my-emojis/",
+        img:"/images/emojis.jpg",
+        link: "/projects/scrimba/my-emojis/index.html",
         blogLink: null,
         isFeatured: false 
     }
@@ -82,8 +85,8 @@ export default[
         id: 9,
         title: "Blackjack",
         description: "Here is a simple text-based Blackjack game. The goal was to create a functioning simulation game with the ability to start a game, and add new cards. I chose to add a simple reset as well as it improves repeat gameplay. CSS and design styling of my own invention. This Javascript project was incrementally more complex in its use of arrays, conditional logic, functions, and use of Math to return random numbers.",
-        img:"images/blackjack-screenshot.jpg",
-        link: "projects/scrimba/blackjack/",
+        img:"/images/blackjack-screenshot.jpg",
+        link: "/projects/scrimba/blackjack/index.html",
         blogLink: null,
         isFeatured: false 
     }
@@ -92,8 +95,8 @@ export default[
         id: 10,
         title: "Passenger Counter",
         description: "For this project the goal was to create a website where a person could log attending visitors as they arrived. The Increment button works as a simple click logger while the Save button adds the previous People entered: value to a string for display below. CSS and design styling of my own invention. This is my first foray into Javascript with Scrimba.",
-        img:"images/passenger-counter.jpg",
-        link: "projects/scrimba/passenger-counter/",
+        img:"/images/passenger-counter.jpg",
+        link: "/projects/scrimba/passenger-counter/index.html",
         blogLink: null,
         isFeatured: false 
     }
@@ -102,8 +105,8 @@ export default[
         id: 11,
         title: "Three Column Preview Card Component",
         description: "This is my solution for the Frontend Mentor Three Column Preview Card Component challenge. For this project participants were instructed to build out a three column card component with active state link buttons to match provided designed images - desktop and mobile. This is my third Frontend Mentor challange.",
-        img:"images/3-col-preview-card-small.jpg",
-        link: "projects/frontendmentor/3-column-preview-card-component/",
+        img:"/images/3-col-preview-card-small.jpg",
+        link: "/projects/frontendmentor/3-column-preview-card-component/index.html",
         blogLink: null,
         isFeatured: false 
     }
@@ -112,8 +115,8 @@ export default[
         id: 12,
         title: "Stats Preview Card Component",
         description: "This is my solution for the Frontend Mentor Stats Preview Card Component challenge. For this project participants were instructed to build out a simple card component to match provided designed images - desktop and mobile. This is my second Frontend Mentor challange.",
-        img:"images/stats_preview_card_desktop_preview_small.jpg",
-        link: "projects/frontendmentor/stats-preview-card-component/",
+        img:"/images/stats_preview_card_desktop_preview_small.jpg",
+        link: "/projects/frontendmentor/stats-preview-card-component/index.html",
         blogLink: null,
         isFeatured: false 
     }
@@ -122,8 +125,8 @@ export default[
         id: 13,
         title: "Order Summary Component",
         description: "This is my solution for the Frontend Mentor Order Summary Component challenge. For this project participants were instructed to build out an order summary card to match provided designed images, including a couple hover states. This is my first Frontend Mentor challange and I really enjoyed it.",
-        img:"images/order-summary.png",
-        link: "projects/frontendmentor/order-summary-component/",
+        img:"/images/order-summary.png",
+        link: "/projects/frontendmentor/order-summary-component/index.html",
         blogLink: null,
         isFeatured: false 
     }
--- portfolio-react-2/src/routes/projects.jsx
+++ portfolio-react-2/src/routes/projects.jsx
@@ -1,10 +1,29 @@
 import React from "react"
+import ProjectData from "../data"
+import ProjectCard from "../components/projectcard"
+import {Link} from "react-router-dom"
 
 export default function Root(){
+    const projectElements = ProjectData.map((project)=>{
+            return <ProjectCard
+            key={project.id}
+            project={project} />
+    })
+
+    console.log("projectElements: ", projectElements)
+
     return(
         <div className="content_wrapper">
             <h1>I am in src/routes/projects.jsx</h1>
             <p>When completed, this will be the project page.</p>
+
+        <div className="">
+            <section className="content_wrapper divider">
+                <h2>Featured projects</h2>
+                <p>View selected projects below. Complete list of projects can be found at <Link to="projects/">https://www.amandatoop.com/projects/</Link>.</p>
+                {projectElements}
+            </section>
+        </div>
         </div>
     )
 }
