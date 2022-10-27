a90b240945581cc255b47f7288e980866104d9ae
Amanda Toop <60271855+atoopdev@users.noreply.github.com>
2022-10-27 10:35:55 -0700

    Added font weights, swapped project image title order

--- portfolio-react-2/src/components/projectcard.jsx
+++ portfolio-react-2/src/components/projectcard.jsx
@@ -2,8 +2,9 @@ import React from "react"
 
 export default function ProjectCard(props){
     return <section className="project-item">
-        <h3 className="project-title">{props.project.title}</h3>
+       
         <a href={props.project.link} className="project-btn"><img src={props.project.img} alt={props.project.title} className="project-img"></img></a>
+        <h3 className="project-title">{props.project.title}</h3>
         <p className="project-description">{props.project.description}</p>
 
         {/* conditional display if there is blog link */}
